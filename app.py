0c993ff523ddc487d2485049149f596e17c8f6ed
cjhorne3 <cjhorne3@asu.edu>
2026-02-08 14:31:11 -0700

    Add page templates and navigation
    Added a shared base template with navigation and implemented home, about, and contact page templates.

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template(
-        "home.html",
-        page_title="Welcome to Chris’s Homepage"
-    )
+    return render_template("home.html", title="Chris's Homepage")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About Chris")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Contact Chris")
 
 if __name__ == "__main__":
     app.run(debug=True)
